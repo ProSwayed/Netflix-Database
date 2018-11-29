9dfbfc1c0b4c57204b4a28bfa1a7a6178b082c72
ProSwayed <44982311+ProSwayed@users.noreply.github.com>
2018-11-29 18:18:18 -0500

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -10,9 +10,9 @@
           DIRECTED_BY
 */
 
-drop database if exists my_defalcot2_netflix;
-create database my_defalcot2_netflix;
-use my_defalcot2_netflix;
+drop database if exists my_USERNAME_netflix;
+create database my_USERNAME_netflix;
+use my_USERNAME_netflix;
 
 create table USER_ACCOUNT(
      id int unsigned primary key auto_increment,
