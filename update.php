f1457611810483f46d9e48e8790b3993f05f7666
ProSwayed <44982311+ProSwayed@users.noreply.github.com>
2018-12-01 16:49:32 -0500

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,15 @@
 <?php
 require_once( 'connect.php' );
 
+$database_val = $databasename;
+
 if( $_REQUEST ) {
 	$type_val = $_REQUEST['type_val'];
 	$id_val = $_REQUEST['id_val'];
+	if( isSet( $_REQUEST['database_val'] ) ) {
+		$database_val = $_REQUEST['database_val'];
+		select_database( $database, $database_val );
+	}
 
 	$stmt = $database->stmt_init();
 	$stmt->prepare( "select * from $type_val where id=?" );
@@ -47,7 +53,7 @@ if( $_POST ) {
 	call_user_func_array( array( $stmt, 'bind_param' ), $arrVals );
 	$stmt->execute();
 
-	header( "Refresh:0; url=update.php?id_val=$id_val&type_val=$type_val" );
+	header( "Refresh:0; url=update.php?id_val=$id_val&type_val=$type_val&database_val=$database_val" );
 }
 ?>
 
@@ -61,6 +67,7 @@ if( $_POST ) {
 		<form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] );?>">
 			<input type="hidden" name="id_val" value="<?php echo $id_val; ?>">
 			<input type="hidden" name="type_val" value="<?php echo $type_val; ?>">
+			<input type="hidden" name="database_val" value="<?php echo $database_val; ?>">
 			<?php foreach( $columns as $col ) { if( field_should_be_visible( $col ) ) { ?>
 				<div class="input-group">
 					<label><?php echo add_space_before_capital( $col ); ?></label>
@@ -71,8 +78,8 @@ if( $_POST ) {
 				<button class="btn" type="submit" name="save">Save</button>
 			</div>
 		</form>
-		<a href="read.php?type_val=<?php echo $type_val; ?>">Read <?php echo to_lower( remove_underscore( $type_val ) ); ?></a>
+		<a href="read.php?type_val=<?php echo $type_val; ?>&database_val=<?php echo $database_val; ?>">Read <?php echo to_lower( remove_underscore( $type_val ) ); ?></a>
 		<br />
-		<a href="index.php">Go Home</a>
+		<a href="index.php?database_val=<?php echo $database_val; ?>">Go Home</a>
 	</body>
 </html>
