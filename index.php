4e7bdb0296f599f4a0e199691e7b1d8904d6ca04
ProSwayed <44982311+ProSwayed@users.noreply.github.com>
2018-11-30 19:23:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
+<?php 
+require_once( 'connect.php' );
+
+$stmt = $database->stmt_init();
+$stmt->prepare( "show tables" );
+$stmt->execute();
+$results = $stmt->get_result();
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>CRUD: Create, Read, Update, Delete</title>
 	</head>
 	<body>
-		<a href="read.php?type=USER_ACCOUNT">USER ACCOUNT</a>
-		<a href="read.php?type=USER_ACCOUNT">USER ACCOUNT</a>
+		<?php while( $row = $results->fetch_array() ) { ?>
+			<a href="read.php?type_val=<?php echo $row[0]; ?>"><?php echo to_lower( remove_underscore( $row[0] ) ); ?></a>
+			<br />
+		<?php } ?>
 	</body>
 </html>
