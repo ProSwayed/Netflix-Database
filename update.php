56f3c47cb8be1f06e69ff32f4e111f577d04304c
ProSwayed <44982311+ProSwayed@users.noreply.github.com>
2018-11-30 19:23:47 -0500

    Update update.php

--- update.php
+++ update.php
@@ -1,36 +1,51 @@
 <?php
-include( 'connect.php' );
+require_once( 'connect.php' );
 
-if( $_GET ) {
-	$id = $_GET['id'];
-	$sql = mysqli_query( $database, "select * from USER_ACCOUNT where id=$id" );
+if( $_REQUEST ) {
+	$type_val = $_REQUEST['type_val'];
+	$id_val = $_REQUEST['id_val'];
 
-	$data = mysqli_fetch_array( $sql );
-	$fname = $data['FirstName'];
-	$lname = $data['LastName'];
-	$email = $data['Email'];
-	$phone = $data['Phone'];
-	$address = $data['StreetAddress'];
-	$city = $data['City'];
-	$state = $data['State'];
-	$zip = $data['Zip'];
-	$password = $data['Salt'];
+	$stmt = $database->stmt_init();
+	$stmt->prepare( "select * from $type_val where id=?" );
+	$stmt->bind_param( 'i', $id_val );
+	$stmt->execute();
+	$data = $stmt->get_result()->fetch_array();
+
+	$stmtcol = $database->stmt_init();
+	$stmtcol->prepare( "show columns from $type_val" );
+	$stmtcol->execute();
+	$colresults = $stmtcol->get_result();
+	while( $col = $colresults->fetch_array() )
+		$columns[] = $col['Field'];
+
+	$new_data = array();
+	foreach( $columns as $col )
+		$new_data[$col] = $data[$col];
 }
 
 if( $_POST ) {
-	$id = $_POST['id'];
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$email = $_POST['email'];
-	$phone = $_POST['phone'];
-	$address = $_POST['address'];
-	$city = $_POST['city'];
-	$state = $_POST['state'];
-	$zip = $_POST['zip'];
-	$password = $_POST['password'];
+	$sql_code = "";
+	$types = '';
 
-	$sql = "update USER_ACCOUNT set Email='$email', FirstName='$fname', LastName='$lname', Phone='$phone', StreetAddress='$address', City='$city', State='$state', Zip='$zip', Salt='$password', Hash=SHA1( '$password' ) where id=$id";
-	test_sqlCode( $database, $sql );
+	foreach( $columns as $col ) {
+		if( field_should_be_visible( $col ) ) {
+			$sql_code .= $col . "=?, ";
+			$types .= gettype( $_POST[$col] )[0];
+			$arrVals[] = &$_POST[$col];
+		}
+		else if( $col == 'Hash' ) {
+			$sql_code .= $col . "=SHA1( ? ), ";
+			$types .= gettype( $_POST['Salt'] )[0];
+			$arrVals[] = &$_POST['Salt'];
+		}
+	}
+	$sql_code = "update $type_val set " . substr( $sql_code, 0, -2 ) . " where id=$id_val";
+
+	$stmt = $database->stmt_init();
+	$stmt->prepare( $sql_code );
+	array_unshift( $arrVals, $types );
+	call_user_func_array( array( $stmt, 'bind_param' ), $arrVals );
+	$stmt->execute();
 }
 ?>
 
@@ -40,52 +55,21 @@ if( $_POST ) {
 		<title>CRUD: Create, Read, Update, Delete</title>
 	</head>
 	<body>
-		<h1>Update User Account</h1>
+		<h1>Update <?php echo to_lower( remove_underscore( $type_val ) ); ?></h1>
 		<form method="post" action="<?php echo htmlspecialchars( $_SERVER["PHP_SELF"] );?>">
-			<div class="input-group">
-				<input type="hidden" name="id" value="<?php echo $id; ?>">
-			</div>
-			<div class="input-group">
-				<label>First Name</label>
-				<input type="text" name="fname" value="<?php echo $fname; ?>">
-			</div>
-			<div class="input-group">
-				<label>Last Name</label>
-				<input type="text" name="lname" value="<?php echo $lname; ?>">
-			</div>
-			<div class="input-group">
-				<label>Email</label>
-				<input type="text" name="email" value="<?php echo $email; ?>">
-			</div>
-			<div class="input-group">
-				<label>Phone Number</label>
-				<input type="text" name="phone" value="<?php echo $phone; ?>">
-			</div>
-			<div class="input-group">
-				<label>Street Address</label>
-				<input type="text" name="address" value="<?php echo $address; ?>">
-			</div>
-			<div class="input-group">
-				<label>City</label>
-				<input type="text" name="city" value="<?php echo $city; ?>">
-			</div>
-			<div class="input-group">
-				<label>State</label>
-				<input type="text" name="state" value="<?php echo $state; ?>">
-			</div>
-			<div class="input-group">
-				<label>Zip</label>
-				<input type="text" name="zip" value="<?php echo $zip; ?>">
-			</div>
-			<div class="input-group">
-				<label>Password</label>
-				<input type="text" name="password" value="<?php echo $password; ?>">
-			</div>
+			<input type="hidden" name="id_val" value="<?php echo $id_val; ?>">
+			<input type="hidden" name="type_val" value="<?php echo $type_val; ?>">
+			<?php foreach( $columns as $col ) { if( field_should_be_visible( $col ) ) { ?>
+				<div class="input-group">
+					<label><?php echo add_space_before_capital( $col ); ?></label>
+					<input type="text" name="<?php echo $col; ?>" value="<?php echo $new_data[$col];?>">
+				</div>
+			<?php } } ?>
 			<div class="input-group">
 				<button class="btn" type="submit" name="save">Save</button>
 			</div>
 		</form>
-		<a href="read.php">Read USER ACCOUNT</a>
+		<a href="read.php?type_val=<?php echo $type_val; ?>">Read <?php echo to_lower( remove_underscore( $type_val ) ); ?></a>
 		<br />
 		<a href="index.php">Go Home</a>
 	</body>
