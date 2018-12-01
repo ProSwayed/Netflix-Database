94d9b79e66fd07ee2b737b79008a1d19cc3d4509
ProSwayed <44982311+ProSwayed@users.noreply.github.com>
2018-12-01 16:49:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php 
 require_once( 'connect.php' );
 
+$database_val = $databasename;
+
 if( $_REQUEST ) {
-	select_database( $database, $_REQUEST['database'] );
+	if( isSet( $_REQUEST['database_val'] ) ) {
+		$database_val = $_REQUEST['database_val'];
+		select_database( $database, $database_val );
+	}
 }
 
 $stmt = $database->stmt_init();
@@ -18,10 +23,12 @@ $results = $stmt->get_result();
 	</head>
 	<body>
 		<?php while( $row = $results->fetch_array() ) { ?>
-			<a href="read.php?type_val=<?php echo $row[0]; ?>"><?php echo to_lower( remove_underscore( $row[0] ) ); ?></a>
+			<a href="read.php?type_val=<?php echo $row[0]; ?>&database_val=<?php echo $database_val; ?>"><?php echo to_lower( remove_underscore( $row[0] ) ); ?></a>
 			<br />
 		<?php } ?>
 		<br />
 		<a href="databases.php">Go to databases</a>
+		<br />
+		<a href="index.html">Go Home</a>
 	</body>
 </html>
