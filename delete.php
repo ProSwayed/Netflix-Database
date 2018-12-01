c62d96c93488504e72dc8904f85e6ef2b32f4d90
ProSwayed <44982311+ProSwayed@users.noreply.github.com>
2018-11-30 19:23:38 -0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,11 +1,16 @@
 <?php 
-include( 'connect.php' );
+require_once( 'connect.php' );
 
 if( $_GET ) {
-	$id = $_GET['id'];
-	$sql = "delete from USER_ACCOUNT where id=$id";
-	test_sqlCode( $database, $sql );
+	$id_val = $_GET['id_val'];
+	$type_val = $_GET['type_val'];
 
-	header( "location: read.php" );
+	$sql = "delete from $type_val where id=?";
+	$stmt = $database->stmt_init();
+	$stmt->prepare( $sql );
+	$stmt->bind_param( 'i', $id_val );
+	$stmt->execute();
+
+	header( "location: read.php?type_val=$type_val" );
 }
 ?>
